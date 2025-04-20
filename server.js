0de192a0677e0b206b2a7994167054ec9115980d
kimah72 <min23005@byui.edu>
2025-04-19 19:26:38 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index.js');
+
+// use the routes
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
