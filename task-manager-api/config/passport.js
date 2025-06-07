ae278ba3ccc9ba9a95dc41c45ca5a6d04ef40a5d
kimah72 <min23005@byui.edu>
2025-06-07 14:18:22 -0600

    Complete Lesson 7 OAuth setup

--- task-manager-api/config/passport.js
+++ task-manager-api/config/passport.js
@@ -2,6 +2,12 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
+console.log('Passport Env:', {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  SESSION_SECRET: process.env.SESSION_SECRET
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -14,8 +20,8 @@ passport.use(
         let user = await User.findOne({ googleId: profile.id });
         if (!user) {
           user = new User({
-            username: profile.displayName,
-            email: profile.emails[0].value,
+            username: profile.displayName || `user_${profile.id}`,
+            email: profile.emails && profile.emails[0] ? profile.emails[0].value : '',
             googleId: profile.id
           });
           await user.save();
